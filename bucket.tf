09c3ee37c87b963810e650e7bdb70ef31e4188f2
piyush771953 <90728945+piyush771953@users.noreply.github.com>
2023-05-06 19:43:37 +0100

    storage bucket1

--- bucket.tf
+++ bucket.tf
@@ -1,6 +1,6 @@
 ﻿
 resource "google_storage_bucket" "example_bucket" {
- Project = "stellar-forest-381414"
+ project_id  = "stellar-forest-381414"
   name          = "piyush-training"
   location      = "EU"
   force_destroy = true
