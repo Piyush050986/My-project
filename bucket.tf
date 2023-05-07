309445307ea903b69d19e2e89f3066584e25e180
piyush771953 <90728945+piyush771953@users.noreply.github.com>
2023-05-07 12:08:47 +0100

    destroy storage bucket project

--- bucket.tf
+++ bucket.tf
@@ -1,7 +1,8 @@
-﻿
+﻿/*
 resource "google_storage_bucket" "example_bucket" {
   name          = "piyush-training"
   project =  "stellar-forest-381414"
   location      = "EU"
   force_destroy = true
 }
+*/
